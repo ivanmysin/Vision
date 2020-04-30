75321117b6095e03d07ba3c4d90e37644b047391
Ivan Mysin <imysin@mail.ru>
2020-04-30 23:46:34 +0300

    Values of A and B points are finded correctly and used mean_intensity for it!

--- main.py
+++ main.py
@@ -1,34 +1,77 @@
 import numpy as np
 from skimage import data # , filters
-from skimage import measure
+from skimage import metrics
+from skimage.filters import gaussian
 from skimage.color import rgb2gray
 import matplotlib.pyplot as plt
 import progect_lib as lib
+from copy import copy
 
 # image = data.astronaut().astype(np.float)  # image = data.astronaut().astype(np.float)  #
 # image = rgb2gray(image)
 # image = image[:250, 100:350] # берем только лицо
 
-image = np.zeros( (201, 201), dtype=float ) # data.camera().astype(np.float)   #
+default_params = {
+    "shift_xy_mean" : True,  # Сдвигать или нет центр гиперколонки
+    "avg_grad_over" : True,  # Усреднять или нет градиент вдоль перпеникуляров к среднему направлению градиента
+    "apply_AB_thresh" : True, # Применять или нет обрезание по средним значениях в граничных гиперколонках
+    "apply_smooth_grad_dist" : True, # Уменьшать ли вес градиента при удалении от центра гиперколонки
+    "apply_grad_in_extrems" : False, # Применять ли градиент, если среднее значение является меньше или больше обоих соседних
+    "smooth_bounds" : True, # не реализовано !!!   Применять или нет сглаживание на границах рецептивных полей
+
+}
+
+params = copy(default_params)
+params["shift_xy_mean"] = True
+params["apply_AB_thresh"] = True
+params["apply_smooth_grad_dist"] = True
+params["avg_grad_over"] = True
+params["apply_grad_in_extrems"] = False
+
+params["outfile"] = "experiment 1"
+
+
+image = 255 * rgb2gray(data.astronaut()).astype(np.float) # data.camera().astype(np.float) #  / 255   # np.zeros( (201, 201), dtype=float ) #
 
 
 Len_y, Len_x = image.shape
 x, y = np.meshgrid(np.linspace(-1, 1, Len_x), np.linspace(1, -1, Len_y))
-weghts_sigm = 0.5
-
-image = -100 * x + 100 * y + 100
-
-centers_x = np.array( [Len_x/2, 126, 103, 148, 102, 144] ) # + 100 # / Len_x * 2 - 1 # np.array( [-0.12, -0.2] ) # np.array( [-0.5, 0.5] ) #
-centers_y = np.array( [Len_y/2, 127, 100, 103, 140, 139] ) # / Len_y * 2 # np.array( [0.5, -0.5] )  # np.array( [0.5, 0.6] )   #
+# y = np.flip(y, axis=0)
 
 
-centers_x, centers_y = lib.pix2rel(centers_x, centers_y, Len_y, Len_x)
+
+# image = -200 * x**2 + 100 + 10 * y**2 # + 100 #
+
+centers_x_255 = np.array( [220, Len_x/2, 126, 103, 148, 102, 144] ) # + 100 # / Len_x * 2 - 1 # np.array( [-0.12, -0.2] ) # np.array( [-0.5, 0.5] ) #
+centers_y_255 = np.array( [120, Len_y/2, 127, 100, 103, 140, 139] ) # / Len_y * 2 # np.array( [0.5, -0.5] )  # np.array( [0.5, 0.6] )   #
+
+
+
+centers_x, centers_y = lib.pix2rel(centers_x_255, centers_y_255, Len_y, Len_x)
 
 centers_x = centers_x[0] # !!!!!
 centers_y = centers_y[0] # !!!!!
+
+x = x - centers_x
+y = y - centers_y
+
+
+x_lim_min = np.min(x)
+x_lim_max = np.max(x)
+
+y_lim_min = np.min(y)
+y_lim_max = np.max(y)
+
 # image = np.zeros_like(image) + 100
 # image[:, :Len_x//2] = 200
-res_image, mean_intensity, onlygrad, mean_x, mean_y, abs_steps, angle_steps, xs_AB, ys_AB, cols_AB = lib.make_preobr(image, centers_x, centers_y, x, y)
+
+
+res_image, mean_intensity, mean_x, mean_y, abs_steps, angle_steps, xs_AB, ys_AB, cols_AB = lib.make_preobr(image, centers_x, centers_y, x, y, params)
+
+
+
+# if use_not_only_grad:
+#     res_image,  _, _, _, _, abs_steps, angle_steps, xs_AB, ys_AB, cols_AB = lib.make_preobr(res_image, centers_x, centers_y, x, y, use_not_only_grad, onlygrad)
 
 # res_image, mean_intensity = lib.make_succades(image, centers_x, centers_y, x, y, weghts_sigm)
 
@@ -42,49 +85,96 @@ fig, ax = plt.subplots(nrows=2, ncols=2 , figsize=(15, 15) )
 
 fig2, ax2 = plt.subplots(nrows=1, ncols=1 , figsize=(15, 15) )
 
-ax[0, 0].imshow(image, cmap="gray", vmin=0, vmax=255)
+ax[0, 0].pcolor(x[0, :], y[:, 0], image, cmap='gray', vmin=0, vmax=255) # imshow(image, cmap="gray", vmin=0, vmax=255)
 ax[0, 0].set_title("Исходное изображение")
-ax[0, 0].scatter(center_x_255, center_y_255, color="red", s=0.5)
+ax[0, 0].scatter(centers_x, centers_y, color="red", s=0.5) #scatter(center_x_255, center_y_255, color="red", s=0.5)
 
-ax[0, 1].imshow(mean_intensity, cmap="gray", vmin=0, vmax=255)
+ax[0, 1].pcolor(x[0, :], y[:, 0], mean_intensity, cmap='gray', vmin=0, vmax=255) # imshow(mean_intensity, cmap="gray", vmin=0, vmax=255)
 ax[0, 1].set_title("Средняя интенсивность")
-ax[0, 1].scatter(center_x_255, center_y_255, color="red", s=0.5)
+ax[0, 1].scatter(centers_x, centers_y, color="red", s=0.5) # scatter(center_x_255, center_y_255, color="red", s=0.5)
 
-nrmse = measure.compare_nrmse(im_true=image, im_test=mean_intensity, norm_type='Euclidean')
-psnr = measure.compare_psnr(im_true=image, im_test=mean_intensity, data_range=255)
-ssim = measure.compare_ssim(image, mean_intensity, data_range=255)
+nrmse = metrics.normalized_root_mse(image_true=image, image_test=mean_intensity)
+psnr = metrics.peak_signal_noise_ratio(image_true=image, image_test=mean_intensity, data_range=255)
+ssim = metrics.structural_similarity(image, mean_intensity, data_range=255)
 ax[0, 1].set_xlabel(label.format(nrmse, psnr, ssim))
 
-ax[1, 0].imshow(onlygrad, cmap="gray", vmin=0, vmax=255)
-ax[1, 0].set_title("Градиент")
-ax[1, 0].scatter(center_x_255, center_y_255, color="red", s=0.5)
-ax[1, 0].scatter(mean_x_255, mean_y_255, color="g", s=5.5)
+# ax[1, 0].imshow(onlygrad, cmap="gray", vmin=0, vmax=255)
+ax[1, 0].set_title("Сетка")
+# ax[1, 0].scatter(center_x_255, center_y_255, color="red", s=0.5)
+# ax[1, 0].scatter(mean_x_255, mean_y_255, color="g", s=5.5)
 
-ax[1, 1].imshow(res_image, cmap="gray", vmin=0, vmax=255)
-ax2.imshow(res_image, cmap="gray", vmin=0, vmax=255)
+ax[1, 1].pcolor(x[0, :], y[:, 0], res_image, cmap='gray', vmin=0, vmax=255) # imshow(res_image, cmap="gray", vmin=0, vmax=255)
+
+ax2.pcolor(x[0, :], y[:, 0], res_image, cmap='gray', vmin=0, vmax=255)
+
 ax[1, 1].set_title("Восстановленное изображение")
-ax[1, 1].scatter(center_x_255, center_y_255, color="red", s=0.5)
-ax2.scatter(center_x_255, center_y_255, color="r", s=0.5)
-ax[1, 1].scatter(mean_x_255, mean_y_255, color="r", s=6.5)
-ax2.scatter(mean_x_255, mean_y_255, color="r", s=6.5)
+# ax[1, 1].scatter(center_x_255, center_y_255, color="red", s=0.5)
+# ax2.scatter(center_x_255, center_y_255, color="r", s=0.5)
+ax[1, 0].scatter(mean_x, mean_y, color="r", s=6.5) # scatter(mean_x_255, mean_y_255, color="r", s=6.5)
+ax2.scatter(mean_x, mean_y, color="r", s=5.5)
 
-xx, yy = lib.get_rete(abs_steps, angle_steps, Len_x, Len_y)
+
+# grad_x_acc = -0.02 * mean_x
+# grad_y_acc = 0.02 * mean_y / 20
+#
+#
+# for x_tmp, y_tmp, dx, dy in zip(mean_x, mean_y, grad_x_acc, grad_y_acc):
+#     ax2.arrow(x_tmp, y_tmp, dx, dy, width = 0.01, head_length = 0.01, color="red")
+
+# centers_x_1, centers_y_1 = lib.rel2pix( centers_x, centers_y, Len_x, Len_y )
+xx, yy = lib.get_rete(abs_steps, angle_steps, Len_x, Len_y, onesformat=True)
 
 for x, y in zip(xx, yy):
-    ax[1, 1].plot(x, y, color="b")
+    #x, y = lib.pix2rel(x, y, Len_x, Len_y)
+
+    ax[1, 0].plot(x, y, color="b")
+    ax[0, 0].plot(x, y, color="b")
     ax2.plot(x, y, color="b")
 
 
 
 for x_AB, y_AB in zip(xs_AB, ys_AB):
-    x_AB, y_AB = lib.rel2pix(x_AB, y_AB, Len_x, Len_y)
-    ax[1, 1].plot(x_AB, y_AB, color="c")
     ax2.plot(x_AB, y_AB, color="c")
+    # x_AB, y_AB = lib.rel2pix(x_AB, y_AB, Len_x, Len_y)
+    ax[1, 0].plot(x_AB, y_AB, color="c")
 
-ax[1, 1].set_xlim(0, Len_x)
-ax2.set_xlim(0, Len_x)
-ax[1, 1].set_ylim(Len_y, 0)
-ax2.set_ylim(Len_y, 0)
+
+ax[1, 0].set_xlim(x_lim_min, x_lim_max)
+ax[1, 0].set_ylim(y_lim_min, y_lim_max)
+
+ax[0, 1].set_xlim(x_lim_min, x_lim_max)
+ax[0, 1].set_ylim(y_lim_min, y_lim_max)
+
+ax[0, 0].set_xlim(x_lim_min, x_lim_max)   # (0, Len_x)
+ax2.set_xlim(x_lim_min, x_lim_max)
+ax[0, 0].set_ylim(y_lim_min, y_lim_max)  # (Len_y, 0)
+ax2.set_ylim(y_lim_min, y_lim_max)
+
+
+
+
+
+
+nrmse = metrics.normalized_root_mse(image_true=image, image_test=res_image)
+psnr = metrics.peak_signal_noise_ratio(image_true=image, image_test=res_image, data_range=255)
+ssim = metrics.structural_similarity(image, res_image, data_range=255)
+ax[1, 1].set_xlabel(label.format(nrmse, psnr, ssim))
+
+resultline = params["outfile"] + " " + label.format(nrmse, psnr, ssim) + "\n"
+
+
+print (resultline)
+
+file = open("./results/metrics.txt", "a")
+file.write(resultline)
+file.close()
+
+fig_saving_path = "./results/" # "./results/old_algorhythm/"
+fig.savefig(fig_saving_path + params["outfile"] + ".png", dpi=200)
+fig2.savefig(fig_saving_path + params["outfile"] + "_large.png", dpi=200)
+# plt.show()
+
+
 
 
 # for cols_x_A, cols_y_A, cols_x_B, cols_y_B, mean_xs, mean_ys in zip(cols_AB[0], cols_AB[1],cols_AB[2], cols_AB[3], cols_AB[4], cols_AB[5] ):
@@ -108,20 +198,4 @@ ax2.set_ylim(Len_y, 0)
 #             x_B, y_B = lib.rel2pix(col_x_B, col_y_B, Len_x, Len_y)
 #             # ax[1, 1].arrow(x_B, y_B, mean_x-x_B, mean_y-y_B, width=0.1, color="r", head_width=1)
 #             ax2.arrow(x_B, y_B, mean_x-x_B, mean_y-y_B, width=0.1, color="r", head_width=1)
-
-
-nrmse = measure.compare_nrmse(im_true=image, im_test=res_image, norm_type='Euclidean')
-psnr = measure.compare_psnr(im_true=image, im_test=res_image, data_range=255)
-ssim = measure.compare_ssim(image, res_image, data_range=255)
-ax[1, 1].set_xlabel(label.format(nrmse, psnr, ssim))
-
-# fig.savefig("./results/astronaut_only_face.png", dpi=500)
-plt.show()
-
-
-
-
-
-
-
 
--- progect_lib.py
+++ progect_lib.py
@@ -21,37 +21,35 @@ def pix2rel(x_255, y_255, Len_x, Len_y):
     y = 1 - y_255 / Len_y * 2
     return x, y
 
-def make_preobr(image, center_x, center_y, x, y):
+def make_preobr(image, center_x, center_y, x, y, params):
 
     delta_x = x[0, 1] - x[0, 0]
     delta_y = y[1, 0] - y[0, 0]
 
-    x = x - center_x
-    y = y - center_y
+    # x = x - center_x
+    # y = y - center_y
 
-
+    # label = 'NRMSE = {:.4f}'
 
     abs_pix = np.sqrt((x**2 + y**2))
     angle_pix = np.arctan2(y, x)
 
 
-
     grad_y, grad_x = np.gradient(image, delta_y, delta_x)
 
-    # print(grad_y)
-    # print("###########################")
-    # print(grad_x)
 
 
+    abs_steps = np.geomspace(0.01*(x.max() - x.min()), np.max(abs_pix)+0.0001, 30)  # np.linspace(np.min(abs_pix), np.max(abs_pix)+0.0001, 10) #
+    angle_steps = np.linspace(-np.pi, np.pi+0.0001, 30)
 
-    abs_steps = np.geomspace(0.01*(x.max() - x.min()), np.max(abs_pix)+0.0001, 6)  # np.linspace(np.min(abs_pix), np.max(abs_pix)+0.0001, 10) #
-    angle_steps = np.linspace(-np.pi, np.pi+0.0001, 6)
+    Len_y, Len_x = image.shape
+    # xx_rete, yy_rete = get_rete(abs_steps, angle_steps, Len_x, Len_y, onesformat=True)
 
     # mask = np.zeros_like(image)
 
     res_image = np.zeros_like(image, dtype=float)
     mean_intensity = np.zeros_like(image, dtype=float)
-    onlygrad = np.zeros_like(image, dtype=float)
+
 
 
     # mean_intensity_list = []
@@ -77,8 +75,8 @@ def make_preobr(image, center_x, center_y, x, y):
 
             # mask[chosen_pix] = np.random.randint(0, 255)
 
-            mean_xs = np.append(mean_xs, np.mean(x[chosen_pix]))
-            mean_ys = np.append(mean_ys, np.mean(y[chosen_pix]))
+            # mean_xs = np.append(mean_xs, np.mean(x[chosen_pix]))
+            # mean_ys = np.append(mean_ys, np.mean(y[chosen_pix]))
 
             mean_intensity[chosen_pix] = np.mean(image[chosen_pix])
             # mean_intensity_list.append(np.mean(image[chosen_pix]))
@@ -91,7 +89,7 @@ def make_preobr(image, center_x, center_y, x, y):
 
     # print(mean_intensity_list.size, np.unique(mean_intensity_list).size)
 
-
+    global_counter = 0
     for idx1, ab_step in enumerate(abs_steps):
         for idx2, an_step in enumerate(angle_steps[:-1]):
 
@@ -100,18 +98,20 @@ def make_preobr(image, center_x, center_y, x, y):
             else:
                 chosen_pix = (abs_pix <= ab_step) & (abs_pix > abs_steps[idx1-1]) & (angle_pix >= an_step) & (angle_pix < angle_steps[idx2+1])
 
-
             if np.sum(chosen_pix) == 0:
                 continue
             elif np.sum(chosen_pix) < 2:
                 mean_intens = np.mean(image[chosen_pix])
                 res_image[chosen_pix] = mean_intens
                 continue
+
+            mean_intens = np.mean(image[chosen_pix])
+
+            if params["avg_grad_over"]:
+                mean_grad_x, mean_grad_y = get_new_mean_grad(grad_x[chosen_pix], grad_y[chosen_pix], x[chosen_pix], y[chosen_pix])
             else:
                 mean_grad_x = np.mean(grad_x[chosen_pix])
                 mean_grad_y = np.mean(grad_y[chosen_pix])
-                mean_intens = np.mean(image[chosen_pix])
-
 
             mean_x = np.mean(x[chosen_pix])
             mean_y = np.mean(y[chosen_pix])
@@ -121,7 +121,12 @@ def make_preobr(image, center_x, center_y, x, y):
             ys_AB.append(y_AB)
 
 
-            val_A, val_B, col_x_A, col_y_A, col_x_B, col_y_B = get_AB_value(x_AB, y_AB, x, y, mean_intensity, chosen_pix, image)
+            val_A, val_B, col_x_A, col_y_A, col_x_B, col_y_B = get_AB_value(x_AB, y_AB, x, y, mean_intensity, chosen_pix, mean_intensity)
+
+            if not params["apply_grad_in_extrems"]:
+                if (val_A > mean_intens and val_B > mean_intens) or (val_A < mean_intens and val_B < mean_intens):
+                    res_image[chosen_pix] = mean_intens
+                    continue
 
 
             cols_x_A.append(col_x_A)
@@ -132,42 +137,92 @@ def make_preobr(image, center_x, center_y, x, y):
             # if  idx1+2 != abs_steps.size:
             #     print("last")
 
-            # print(np.sum(chosen_pix))
-            # z = 10*np.ones_like(x)
-            # z[chosen_pix] = 5
-            # fig, ax = plt.subplots(nrows=1, ncols=1)
-            # ax.pcolor(x[0], y[:, 0], z, cmap='gray', vmin=0, vmax=10)
-            # ax.plot(x_AB, y_AB, color="green")
-            # ax.scatter(mean_x, mean_y, color="m")
-            # ax.scatter(col_x_A, col_y_A, color="r", s=50)
-            # ax.scatter(col_x_B, col_y_B, color="r", s=50)
-            # plt.show(fig)
+            mean_x_new = mean_x
+            mean_y_new = mean_y
+
+            if val_A - val_B != 0: #  or val_A > mean_intens and
+
+                if params["shift_xy_mean"]:
+                    mean_x_new = (val_A*x_AB[0] - val_B*x_AB[1] + mean_intens*(x_AB[1] - x_AB[0]) ) / (val_A - val_B)
+                    mean_y_new = (val_A*y_AB[0] - val_B*y_AB[1] + mean_intens*(y_AB[1] - y_AB[0]) ) / (val_A - val_B)
+
+                grad_component = mean_grad_x * (x[chosen_pix] - mean_x_new) + mean_grad_y * (y[chosen_pix] - mean_y_new)
+
+                if params["apply_smooth_grad_dist"]:
+                    grad_component_weights = np.exp( -100.0 * ( (mean_x_new - x[chosen_pix] )**2 + (mean_y_new - y[chosen_pix] )**2  )  )
+                    grad_component_weights /= np.max(grad_component_weights)
+                else:
+                    grad_component_weights = 1.0
+
+                res_image[chosen_pix] = mean_intens + grad_component_weights*grad_component
 
 
-            if val_A - val_B != 0:
-                mean_x = (val_A*x_AB[0] - val_B*x_AB[1] + mean_intens*(x_AB[1] - x_AB[0]) ) / (val_A - val_B)
-                mean_y = (val_A*y_AB[0] - val_B*y_AB[1] + mean_intens*(y_AB[1] - y_AB[0]) ) / (val_A - val_B)
-
-                res_image[chosen_pix] = mean_intens + mean_grad_x * (x[chosen_pix] - mean_x) + mean_grad_y * (y[chosen_pix] - mean_y)
-                onlygrad[chosen_pix] = mean_grad_x * (x[chosen_pix] - mean_x) + mean_grad_y * (y[chosen_pix] - mean_y)
-
-                res_image_tmp = res_image[chosen_pix]
-                res_image_tmp[res_image_tmp <= val_A] = val_A
-                res_image_tmp[res_image_tmp >= val_B] = val_B
-                res_image[chosen_pix] = res_image_tmp
+                if params["apply_AB_thresh"]:
+                    res_image_tmp = res_image[chosen_pix]
+                    res_image_tmp[res_image_tmp <= val_A] = val_A
+                    res_image_tmp[res_image_tmp >= val_B] = val_B
+                    res_image[chosen_pix] = res_image_tmp
 
             else:
                 print("Ua and Ub are equal!")
-                # res_image[chosen_pix] = 0
-                # print(mean_x)
-                # print(mean_y)
-
                 res_image[chosen_pix] = mean_intens
 
+            mean_xs = np.append(mean_xs, mean_x_new)
+            mean_ys = np.append(mean_ys, mean_y_new)
 
+            # print(np.sum(chosen_pix))  Антоха, все отправил?
+            # z = np.ones_like(image)
+            # z[chosen_pix] = res_image[chosen_pix]
+            # fig, ax = plt.subplots(nrows=1, ncols=1)
+            # ax.pcolor(x[0, :], y[:, 0], z, cmap='gray', vmin=0, vmax=1)
+            # # print(y[:, 0])
+            # ax.plot(x_AB, y_AB, color="green")
+            # ax.scatter(mean_x, mean_y, color="red")
+            # ax.scatter(mean_x_new, mean_y_new, color="blue")
+            # ax.scatter(col_x_A, col_y_A, color="m", s=50)
+            # ax.scatter(col_x_B, col_y_B, color="m", s=50)
+            # nrmse = np.sqrt( np.mean( (res_image[chosen_pix] - image[chosen_pix])**2 ) )
+            # ax.set_xlabel(label.format(nrmse))
+            #
+            # x_min2plot = np.min( x[chosen_pix] ) - delta_x
+            # x_max2plot = np.max( x[chosen_pix] ) + delta_x
+            #
+            # x_min2plot -= 0.2*np.abs(x_min2plot)
+            # x_max2plot += 0.2*np.abs(x_max2plot)
+            #
+            # # print(x_min2plot, x_max2plot)
+            # ax.set_xlim(x_min2plot, x_max2plot)
+            #
+            # y_min2plot = np.min(y[chosen_pix]) - delta_y # - 0.2 * np.abs(np.min(y[chosen_pix]))
+            # y_max2plot = np.max(y[chosen_pix]) + delta_y # + 0.2 * np.abs(np.max(y[chosen_pix]))
+            #
+            # y_min2plot -= 0.2 * np.abs(y_min2plot)
+            # y_max2plot += 0.2 * np.abs(y_max2plot)
+            #
+            #
+            # ax.set_ylim(y_min2plot, y_max2plot)
+            #
+            # for x_rete, y_rete in zip(xx_rete, yy_rete):
+            #     ax.plot(x_rete, y_rete, color="b")
+            #
+            #
+            # fig.savefig("./results/each_segment/" + str(global_counter)  + ".png")
+            # # plt.show(block=False)
+            # # # break
+            # plt.close(fig)
+
+
+            global_counter += 1
+
+    # cols_x_A = np.asarray(cols_x_A) + center_x
+    # cols_y_A = np.asarray(cols_y_A) + center_y
+    # cols_x_B = np.asarray(cols_x_B) + center_x
+    # cols_y_B = np.asarray(cols_y_B) + center_y
+    # mean_xs = np.asarray(mean_xs) + center_x
+    # mean_ys = np.asarray(mean_ys) + center_y
 
     cols_AB = [cols_x_A, cols_y_A, cols_x_B, cols_y_B, mean_xs, mean_ys]
-    return res_image, mean_intensity, onlygrad, mean_xs, mean_ys, abs_steps, angle_steps, xs_AB, ys_AB, cols_AB   # , mask
+    return res_image, mean_intensity, mean_xs, mean_ys, abs_steps, angle_steps, xs_AB, ys_AB, cols_AB   # , mask
 
 
 
@@ -208,44 +263,52 @@ def make_succades(image, centers_x, centers_y, x, y, weghts_sigm):
 
     return res_image, mean_intensity
 
-def get_rete(abs_steps, angle_steps, Len_x, Len_y):
+def get_rete(abs_steps, angle_steps, Len_x, Len_y, centr_x = 0, centr_y = 0,  onesformat=False):
     xx = []
     yy = []
 
     for ab in abs_steps:
         x = np.linspace(-ab, ab, 1000)
-        y = np.sqrt(ab ** 2 - x ** 2)
-        xx.append(x)
-        xx.append(x)
-        yy.append(y)
-        yy.append(-y)
+        y = np.sqrt(ab**2 - x**2)
+        xx.append( np.copy(x) )
+        xx.append( np.copy(x) )
+        yy.append(y + centr_y )
+        yy.append(-y + centr_y)
 
     for an in angle_steps:
         x = 0
         # print(an)
         if np.abs(an) == np.pi:
-            x = np.array([-1, 0], dtype=float)
-            y = np.zeros_like(x)
+            x = np.array([-2, 0], dtype=float)
+            y = np.zeros_like(x) + centr_y
         elif an == 0:
-            x = np.array([0, 1], dtype=float)
-            y = np.zeros_like(x)
+            x = np.array([0, 2], dtype=float)
+            y = np.zeros_like(x) + centr_y
         elif an == np.pi / 2:
             x = np.array([0, 0], dtype=float)
-            y = np.linspace(0, 1, 2)
+            y = np.linspace(0, 2, 2) + centr_y
         elif an == -np.pi / 2:
             x = np.array([0, 0], dtype=float)
-            y = np.linspace(-1, 0, 2)
+            y = np.linspace(-2, 0, 2)  + centr_y
         elif np.abs(an) < np.pi/2:
-            x = np.array([0, 1], dtype=float)
+            x = np.array([0, 2], dtype=float)
             k = np.tan(an)
-            y = k * x
+            y = k * x + centr_y
         elif np.abs(an) > np.pi/2:
-            x = np.array([-1, 0], dtype=float)
+            x = np.array([-2, 0], dtype=float)
             k = np.tan(an)
-            y = k * x
+            y = k * x  + centr_y
         if not (type(x) is int):
             xx.append(x)
             yy.append(y)
+
+
+    for x in xx:
+        x += centr_x
+
+    if onesformat:
+        return xx, yy
+
 
     for idx, (x, y) in enumerate(zip(xx, yy)):
         x_255, y_255 = rel2pix(x, y, Len_x, Len_y)
@@ -257,7 +320,7 @@ def get_rete(abs_steps, angle_steps, Len_x, Len_y):
 
 def get_distance2AB(fi_min, fi_max, abs_min, abs_max, grad_x, grad_y, x_mean, y_mean):
 
-    if grad_y == 0:
+    if grad_x == 0:
         x = np.asarray([x_mean, x_mean])
 
         tmp = np.array([abs_min, abs_max])**2  - x**2
@@ -272,7 +335,7 @@ def get_distance2AB(fi_min, fi_max, abs_min, abs_max, grad_x, grad_y, x_mean, y_
         y = np.append(y, x[0:2]*np.tan([fi_min, fi_max]))
 
     else:
-        tan_fi_grad = grad_x / grad_y
+        tan_fi_grad = grad_y / grad_x
         alpha = -tan_fi_grad*x_mean + y_mean
 
         x = np.empty(0, dtype=np.float)
@@ -362,3 +425,43 @@ def get_AB_value(x_AB, y_AB, pix_x, pix_y, mean_intensity, chosen_pix, original_
 
     # print(value_A, value_B)
     return value_A, value_B, col_x_A, col_y_A, col_x_B, col_y_B
+
+
+def get_new_mean_grad(grad_x, grad_y, x, y):
+
+    mean_grad_x = np.mean(grad_x)
+    mean_grad_y = np.mean(grad_y)
+
+    ampl_mean = np.sqrt(mean_grad_x**2 + mean_grad_y**2)
+
+    if ampl_mean < 0.0001:
+        return mean_grad_x, mean_grad_y
+
+
+    mean_grad_x /= ampl_mean
+    mean_grad_y /= ampl_mean
+
+    project_on_mean_grad = grad_x * mean_grad_x + grad_y * mean_grad_y
+
+    if grad_x.size < 15:
+        max_proj = np.max(project_on_mean_grad)
+        mean_grad_x = mean_grad_x * max_proj
+        mean_grad_y = mean_grad_y * max_proj
+
+        return mean_grad_x, mean_grad_y
+
+    grad_angle = np.arctan2(mean_grad_y, mean_grad_x)
+
+    # print( np.rad2deg( grad_angle) )
+    x_ = x * np.cos(grad_angle) + y * np.sin(grad_angle)
+    bins = 10 # np.arange(np.min(x_), np.max(x_), step_x_) #!!!!!! step = np.median( np.diff( np.sort(x) ) )
+
+    ones_hist =  np.histogram(x_, bins=bins)[0]
+    weighted_hist =  np.histogram(x_, weights=project_on_mean_grad, bins=bins)[0]
+    non_zeros = ones_hist != 0
+    ampl = np.max( weighted_hist[non_zeros] / ones_hist[non_zeros] )
+
+    mean_grad_x *= ampl
+    mean_grad_y *= ampl
+
+    return mean_grad_x, mean_grad_y
