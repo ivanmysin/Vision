4a31f66ab66fa8381ca32af39d73d685be3fcda4
Ivan Mysin <imysin@mail.ru>
2020-09-25 00:33:32 +0300

    New algorhythm with convolution and deconvolution

--- conv1d.py
+++ conv1d.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.signal import deconvolve, ricker
+from scipy.interpolate import interp1d
 from numpy.fft import fft, ifft, ifftshift
 def wiener_deconvolution(signal, kernel, lambd):
     "lambd is the SNR"
@@ -19,7 +20,7 @@ ends[cuted<0] *=  np.exp( -0.5*(cuted[cuted<0] /sigma_ends)**2 )
 cuted = t - t[-1] + 5*sigma_ends
 ends[cuted>0] *=  np.exp( -0.5*(cuted[cuted>0] /sigma_ends)**2 )
 
-u = np.cos(2*np.pi*200.5*t)
+u = np.cos(2*np.pi*15.5*t)
 u *= ends
 
 
@@ -34,13 +35,25 @@ kernel = np.exp(-0.5 * (tker/sigma1)**2)/sigma1 - np.exp(-0.5 * (tker/sigma2)**2
 
 coded = np.convolve(u, kernel, mode="full")
 
+coded_ = coded[35:-36:30]
+t_ = t[::30]
+t_[-1] = t[-1]
+
+f = interp1d(t_, coded_, kind='cubic')
 
 
+coded_ = f(t)
 
-deconv = wiener_deconvolution(coded, kernel, 0)
+coded_ = np.append(np.zeros(34), coded_)
+coded_ = np.append(coded_, np.zeros(35))
+
+deconv = wiener_deconvolution(coded_, kernel, 0)
+deconv = deconv[:1-kernel.size]
 print(deconv.shape)
 
-plt.plot(deconv)
+
+plt.plot(deconv, color="green", linewidth=5)
+plt.plot(u, color="red")
 #plt.ylim(-1, 1)
 plt.show()
 
