bd4ea4071b9ebc4a0ae5f313c9044c8f5f6ffbc7
Ivan Mysin <imysin@mail.ru>
2020-10-21 22:38:54 +0300

    Coded and decoded by sines are debugged

--- wavelets2.py
+++ wavelets2.py
@@ -1,10 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from elephant.signal_processing import wavelet_transform
+import sys
+
+sys.path.append("../")
+import pycwt.pycwt as pycwt
 
 
 t = np.linspace(0, 1, 900)
-fs = 1 / (t[1] - t[0])
+dt = (t[1] - t[0])
+
 sigma_ends = 0.01
 ends = np.ones_like(t)
 cuted = t - t[0] - 5*sigma_ends
@@ -15,16 +19,16 @@ ends[cuted>0] *=  np.exp( -0.5*(cuted[cuted>0] /sigma_ends)**2 )
 u = np.cos(2*np.pi*15*t)
 u *= ends
 
-
-
 frequencies = np.arange(1, 51, 1)
-wavelet_u = wavelet_transform(u, frequencies, fs=fs)
+wavelet_u_list = pycwt.cwt(u, dt, freqs=frequencies)
+
+wavelet_u = wavelet_u_list[0]
 
 wave_u_abs = np.abs(wavelet_u) #*2/ u.size
 
 
 
-fig, axes = plt.subplots(nrows=3, sharex=True)
+fig, axes = plt.subplots(nrows=4, sharex=True)
 axes[0].plot(t, u)
 axes[0].set_xlim(0, 1)
 
@@ -41,25 +45,33 @@ for idx in range(30, u.size+30, 30):
     t_col_centered = tcol - tcol_c
     
    
-    wave_abs_cols = wave_u_abs[:, sl] # * (1 + 0.0001 * np.abs(t_col_centered))
+    wave_abs_cols = wave_u_abs[:, sl] * np.exp(-1000 * t_col_centered**2)
     max_idx = np.unravel_index(np.argmax(wave_abs_cols), wave_abs_cols.shape)
     # print( frequencies[max_idx[0]], t[sl][max_idx[1]] )
     
     axes[0].vlines(tcol[-1], -2, 2)
     axes[0].scatter( t[sl][max_idx[1]], [1, ], color="red", s=5 )
     
-    freq_col = frequencies[max_idx[0]]
+    # freq_col = frequencies[max_idx[0]]
     
-    amp_col = wave_abs_cols[max_idx[0], max_idx[1]]
-    phase_col = np.angle( wavelet_u[:, sl][max_idx[0], max_idx[1]] )
-    # print(amp_col)
-    decoded = amp_col * np.cos(2 * np.pi * t_col_centered * freq_col + phase_col) 
+ 
+    wavelet_u[:max_idx[0], sl] = 0 + 0*1j
+    wavelet_u[max_idx[0]+1:, sl] = 0 + 0*1j
+
     
-    # decoded = wave_abs_cols * np.cos(2 * np.pi * tcol * freq_col + phase_col) 
+    wavelet_u[max_idx[0], sl] = wavelet_u[max_idx[0], sl] / wave_u_abs[max_idx[0], sl] * wave_abs_cols[max_idx[0], max_idx[1]]
     
     
-    axes[2].plot(tcol, decoded)
-    axes[2].vlines(tcol[-1], -2, 2)
-    axes[2].scatter( t[sl][max_idx[1]], [1, ], color="red", s=5 )
+    
+    axes[3].vlines(tcol[-1], -2, 2)
+    axes[3].scatter( t[sl][max_idx[1]], [1, ], color="red", s=5 )
+
+wave_u_abs = np.abs(wavelet_u) #*2/ u.size
+axes[2].pcolor(t, frequencies, wave_u_abs)
+
+decode = pycwt.icwt(wavelet_u, wavelet_u_list[1], dt)
+axes[3].plot(t, decode)
+
+
 
 plt.show()
