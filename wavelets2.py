3b81beda3a7add7c290d87323e8dc3f57755fcf6
Ivan Mysin <imysin@mail.ru>
2020-11-08 18:27:13 +0300

    mexican hat wavelets are used

--- wavelets2.py
+++ wavelets2.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.signal import hilbert
 import sys
-
 sys.path.append("../")
 import pycwt.pycwt as pycwt
 
@@ -17,24 +17,24 @@ ends[cuted<0] *=  np.exp( -0.5*(cuted[cuted<0] /sigma_ends)**2 )
 cuted = t - t[-1] + 5*sigma_ends
 ends[cuted>0] *=  np.exp( -0.5*(cuted[cuted>0] /sigma_ends)**2 )
 
-w = 400 # np.linspace(2, 25, t.size) # 15 # 
+w = 10 # np.linspace(2, 25, t.size) # 15 # 
 u = np.cos(2*np.pi*w*t) # + 0.9*np.cos(2*np.pi*5*t)
 u *= ends
 
-frequencies = np.arange(1, 448, 1)
-wavelet_u_list = pycwt.cwt(u, dt, freqs=frequencies)
+frequencies = np.arange(1, 50, 1)
+wavelet_u_list = pycwt.cwt(u, dt, freqs=frequencies, wavelet='mexicanhat') 
 
-wavelet_u = wavelet_u_list[0]
+wavelet_u = hilbert( wavelet_u_list[0].real, axis=1)
 
 wave_u_abs = np.abs(wavelet_u) #*2/ u.size
 
 
 
-fig, axes = plt.subplots(nrows=4, sharex=True, figsize=(20, 10) )
+fig, axes = plt.subplots(nrows=4, sharex=True, figsize=(5, 5) )
 axes[0].plot(t, u)
 axes[0].set_xlim(0, 1)
 
-axes[1].pcolor(t, frequencies, wave_u_abs)
+axes[1].pcolor(t, frequencies, wave_u_abs, shading='auto')
 
 
 
@@ -46,32 +46,48 @@ for idx in range(30, u.size+30, 30):
 
     t_col_centered = tcol - tcol_c
     
-   
+    wave_u_col = wavelet_u[:, sl]
     wave_abs_cols = wave_u_abs[:, sl] 
-    max_idx = np.unravel_index(np.argmax(wave_abs_cols * np.exp(-0.0000001 * t_col_centered**2)), wave_abs_cols.shape)
+    max_idx = list(np.unravel_index(np.argmax(wave_abs_cols * np.exp(-1e-5 * t_col_centered**2)), wave_abs_cols.shape))
+    
+    # maximums_indexes = np.argwhere(wave_abs_cols[max_idx[0], :] == wave_abs_cols[max_idx[0], max_idx[1]])
+    # maximums_indexes = np.asarray(maximums_indexes).ravel()
+    # print(maximums_indexes)
+    # max_idx[0] = np.argmax( np.abs(maximums_indexes - 15) )
+    
+    
     # print( frequencies[max_idx[0]], t[sl][max_idx[1]] )
     
-    axes[0].vlines(tcol[-1], -2, 2)
-    axes[0].scatter( t[sl][max_idx[1]], [1, ], color="red", s=5 )
+    axes[0].vlines(tcol[-1], -2, 2, color="black")
+    axes[0].scatter( tcol[max_idx[1]], [1.2, ], color="red", s=5 )
     
-    # freq_col = frequencies[max_idx[0]]
     
- 
+
     wavelet_u[:max_idx[0], sl] = 0 + 0*1j
     wavelet_u[max_idx[0]+1:, sl] = 0 + 0*1j
 
+    # wavelet_u[max_idx[0], sl] = wavelet_u[max_idx[0], sl] / wave_u_abs[max_idx[0], sl] * wave_abs_cols[max_idx[0], max_idx[1]]
+    # wavelet_u[max_idx[0], sl][0:max_idx[1]] = 0 + 0*1j
+    # wavelet_u[max_idx[0], sl][max_idx[1]+1 :] = 0 + 0*1j
     
-    wavelet_u[max_idx[0], sl] = wavelet_u[max_idx[0], sl] / wave_u_abs[max_idx[0], sl] * wave_abs_cols[max_idx[0], max_idx[1]]
-    
-    
-    
-    axes[3].vlines(tcol[-1], -2, 2)
-    axes[3].scatter( t[sl][max_idx[1]], [1, ], color="red", s=5 )
 
-wave_u_abs = np.abs(wavelet_u) #*2/ u.size
-axes[2].pcolor(t, frequencies, wave_u_abs)
+    phi_col_cent = np.angle(wave_u_col[max_idx[0], max_idx[1]])
+    len_col_cent = wave_abs_cols[max_idx[0], max_idx[1]]
+    freq_col = frequencies[max_idx[0]]
+    tcol_max = tcol[max_idx[1]]
+    
+    wavelet_u[max_idx[0], sl] = len_col_cent * np.exp(1j*(freq_col*2*np.pi*(tcol-tcol_max) + phi_col_cent))
+    #  
+    
+    
 
-decode = pycwt.icwt(wavelet_u, wavelet_u_list[1], dt)
+    axes[3].vlines(tcol[-1], -2, 2, color="black")
+    axes[3].scatter( tcol[max_idx[1]], [1.2, ], color="red", s=5 )
+
+wave_u_abs = np.abs(wavelet_u) 
+axes[2].pcolor(t, frequencies, wave_u_abs, shading='auto')
+
+decode = pycwt.icwt(wavelet_u, wavelet_u_list[1], dt, wavelet='mexicanhat') # 
 axes[3].plot(t, decode)
 
 
