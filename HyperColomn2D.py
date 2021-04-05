77bf53ae3d0881aa4b3b4acc20306286c5562b53
Ivan Mysin <imysin@mail.ru>
2021-04-05 18:41:32 +0300

    Hypercolomn class is started to developed

--- HyperColomn2D.py
+++ HyperColomn2D.py
@@ -1,17 +1,22 @@
 import numpy as np
-import matplotlib.pyplot as plt
 from scipy.optimize import minimize
+from scipy.signal import convolve2d as convolve2d
 
 class HyperColomn:
-    def __init__(self, centers, xx, yy, angles, sigmas):
+    def __init__(self, centers, xx, yy, angles, sigmas, frequencies=None):
 
         self.cent_x = centers[0]
         self.cent_y = centers[1]
         self.xx = xx - self.cent_x
         self.yy = yy - self.cent_y
 
+
+
         self.x = self.xx[0, :]
         self.y = self.yy[:, 0]
+
+        self.cent_x_idx = np.argmin( np.abs(self.x) )
+        self.cent_y_idx = np.argmin( np.abs(self.y) )
 
         self.dx = self.x[1] - self.x[0]
         self.dy = self.y[1] - self.y[0]
@@ -21,6 +26,20 @@ class HyperColomn:
 
         self.aproximate_hilbert_kernel()
 
+        self.rot_xx = []
+        self.rot_yy = []
+
+        if frequencies is None:
+            self.frequencies = np.geomspace(0.1, 102.4, num=11)
+        else:
+            self.frequencies = frequencies
+
+        self.mexican_hats = []
+        self.hilbert_aproxed_f = []
+        self.hilbert_aproxed_b = []
+
+        self.rotate_xy()
+        self.compute_kernels()
 
     def aproximate_hilbert_kernel(self):
         w0 = np.ones(self.sigmas.size, dtype=np.float)
@@ -31,10 +50,35 @@ class HyperColomn:
         self.H_aproxed_normed = self.H_aproxed / np.sqrt(np.sum(self.H_aproxed ** 2))
 
     def rotate_xy(self):
-        pass
+        for phi in self.angles:
+            xx = self.xx * np.cos(phi) - self.yy * np.sin(phi)
+            yy = self.xx * np.sin(phi) + self.yy * np.cos(phi)
+
+            self.rot_xx.append(xx)
+            self.rot_yy.append(yy)
 
     def compute_kernels(self):
-        pass
+        for phi_idx in range(len(self.angles)):
+
+            xx = self.rot_xx[phi_idx] + self.dx
+            yy = self.rot_yy[phi_idx]
+            H_aprox = self.sum_gaussian_derivaries(self.dg_weiths, xx, yy, self.sigmas)
+            H_aprox = H_aprox / np.sqrt(np.sum(H_aprox**2))
+
+            self.hilbert_aproxed_f.append(H_aprox)
+
+            xx = self.rot_xx[phi_idx] - self.dx
+            H_aprox = self.sum_gaussian_derivaries(self.dg_weiths, xx, yy, self.sigmas)
+            H_aprox = H_aprox / np.sqrt(np.sum(H_aprox**2))
+
+            self.hilbert_aproxed_b.append(H_aprox)
+
+            self.mexican_hats.append([])
+            for freq in self.frequencies:
+                sigma = 1 / (np.sqrt(2) * np.pi * freq)
+                hat = self.get_rickers(sigma, xx, yy)
+                self.mexican_hats[phi_idx].append(hat)
+
 
     def get_rickers(self, sigma, xx, yy):
         ricker = 0.005 * (4 - xx**2 / sigma**2) * np.exp(-(xx**2 + 4 * yy**2) / (8 * sigma**2)) / sigma**4
@@ -44,8 +88,6 @@ class HyperColomn:
         gaussian = np.exp(-(xx**2 + 4 * yy**2) / (8 * sigma**2)) / (4 * np.pi * sigma**2)
         gaussian_dx = gaussian * -0.25 * xx / (sigma**2)
         return gaussian_dx
-
-
 
     def sum_gaussian_derivaries(self, w, xx, yy, sigmas):
         dgsum = np.zeros_like(xx)
@@ -61,9 +103,39 @@ class HyperColomn:
 
         return E
 
+    def transform(self, U):
+        
+        U_restored = np.zeros_like(U)
+        
+        for phi_idx in range(len(self.angles)):
+            # u_imag_f = np.sum(U * self.hilbert_aproxed_f[phi_idx])
+            # u_imag_b = np.sum(U * self.hilbert_aproxed_b[phi_idx])
+            u_imag = convolve2d(U, self.hilbert_aproxed_b[phi_idx], mode="same")
 
+            u = U + 1j*u_imag
+
+            freq_idx = 8
+            dx = self.dx*np.cos(self.angle[phi_idx]) - self.dy*np.sin(self.angle[phi_idx])
+            # cent_x_idx_b = np.argmin( np.abs(-dx) )
+            # cent_x_idx_f = np.argmin( np.abs(dx) )
+
+
+            # for freq_idx, freq in enumerate(self.frequencies): # !!!!!!!1
+            Ucoded = convolve2d(u, self.mexican_hats[phi_idx][freq_idx], mode="same")
+            phase = np.angle(Ucoded[self.cent_y_idx, self.cent_x_idx])
+
+
+
+
+
+
+            print(phase)
+            break
+        return U_restored
 
 if __name__ == '__main__':
+    import matplotlib.pyplot as plt
+
     centers = [0, 0]
     centers = [0, 0]
     angles = [0, 0.5*np.pi]
@@ -75,8 +147,12 @@ if __name__ == '__main__':
     sigmaximum = 0.005
     sigmas = np.linspace(sigminimum, sigmaximum, nsigmas)
     xx, yy = np.meshgrid(np.linspace(-0.5, 0.5, Len_y), np.linspace(0.5, -0.5, Len_x))
+    image = np.cos(2*np.pi*xx*5)
 
-    hc = HyperColomn(centers, xx, yy, angles,sigmas)
 
-    print(hc.H_aproxed_normed.shape)
-    print("Hello")
+    hc = HyperColomn(centers, xx, yy, angles, sigmas)
+    hc.transform(image)
+
+    plt.pcolormesh(hc.x, hc.y, hc.mexican_hats[1][8], cmap="rainbow", shading="auto")
+
+    plt.show()
--- test.py
+++ test.py
@@ -2,6 +2,13 @@ import numpy as np
 from scipy.signal import hilbert
 import matplotlib.pyplot as plt
 
+
+x = np.array([1, 2, 3, 4])
+
+y = np.fft.fft(x)
+
+print(y)
+"""
 def myfft_convolve(x1, x2, mode="full"):
     
     n1 = x1.size
@@ -38,81 +45,4 @@ c2 = myfft_convolve(a, b, mode="valid")
 print(c1)
 print(c2)
 
-
-
-"""
-points = 10000
-
-xvect = np.linspace(-10, 10, points)    #np.arange(0, points) - (points - 1.0) / 2
-wp = 2 * np.pi * 0.03
-
-dx = xvect[1] - xvect[0]
-sigma = 0.5 # 1.0 / wp
-a = 0.5 / sigma**2
-
-
-kernel = np.sqrt(a / np.pi) * np.exp(-a * xvect**2)  * (-2.0 * a * xvect)
-
-# kernel = hilbert(kernel).imag
-# kernel = kernel / ( np.max(kernel) - np.min(kernel) )
-
-# print(np.sum(kernel))
-# print(np.sum(kernel**2))
-kernel_fft = np.fft.fft(kernel)
-kernel_fft = np.fft.fftshift(kernel_fft)
-#kernel_fft = np.abs(kernel_fft) / np.sum(np.abs(kernel_fft) )
-freqs = np.fft.fftshift( np.fft.fftfreq(xvect.size, xvect[1]-xvect[0]) )
-
-
-theor_spec = freqs * 1j * np.exp(-(np.pi * freqs)**2 / a) * np.sqrt(np.pi / a) 
-#theor_spec = np.abs(theor_spec) / np.sum(np.abs(theor_spec) )
-
-plt.plot(freqs, kernel_fft.real, color="green", linewidth=5)
-# plt.plot(freqs, kernel_fft.real)
-# plt.plot(freqs, kernel_fft.imag)
-plt.plot(freqs, theor_spec.real, color="blue")
-# plt.plot(freqs, theor_spec.real)
-# plt.plot(freqs, theor_spec.imag)
-
-
-plt.figure()
-plt.plot(xvect, kernel)
-
-
-
-plt.show()
-"""
-
-"""
-points = 100
-
-xvect = np.arange(0, points) - (points - 1.0) / 2
-wp = 2 * np.pi * 0.05
-
-a = np.sqrt(2) / wp
-A = 2 / (np.sqrt(3 * a) * (np.pi**0.25))
-wsq = a**2
-vec = np.arange(0, points) - (points - 1.0) / 2
-xsq = vec**2
-mod = (1 - xsq / wsq)
-gauss = np.exp(-xsq / (2 * wsq))
-
-ricker = A * mod * gauss
-
-ricker_fft = 2 * np.abs( np.fft.rfft(ricker) ) / ricker.size
-
-freqs = np.fft.rfftfreq(ricker.size, xvect[1]-xvect[0])
-print( np.mean(np.abs(np.fft.fft(ricker)**2) ))
-print( np.sum(ricker**2) )
-
-
-wp = wp / 2 / np.pi
-R = 2 * (freqs**2) / (np.sqrt(np.pi) * wp**3) * np.exp(-(freqs**2/wp**2) )
-R = R / np.sum(R)
-ricker_fft = ricker_fft / np.sum(ricker_fft)
-
-
-plt.plot(freqs, ricker_fft)
-plt.plot(freqs, R)
-plt.show()
 """
